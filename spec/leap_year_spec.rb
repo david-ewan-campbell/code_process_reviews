15ea3c93edd7776887167fbb158db88dfd34e621
David Ewan Campbell <ewan.david@gmail.com>
2022-10-20 14:23:06 +0100

    Homebrew, rvm, ruby update,  gems, rspec installed & 1st test receiving expected fail as hoped

---
+++ spec/leap_year_spec.rb
@@ -0,0 +1,19 @@
+require('rspec')
+require('leap_year')
+
+describe('leap_year?') do
+  it("returns false for a year not divisible by 4") do
+    expect(leap_year?(1993)).to(eq(false))
+  end
+end
+
+# Not adding ends automatically?
+=begin
+Step 1. Number to be divisible by 4 to be a leap year
+Step 2. If number divisible by 100 then not a leap year
+Step 3. However if divisible by 400 also a leap year
+=end
+
+# Remember to run rspec tests from main folder - 
+# 1st test should give 'No method error'
+# Adding empty method to lib file gives a different 'nil error'
