6fa73c4181ac3e308ca8ee459024ce16c312565f
David Ewan Campbell <ewan.david@gmail.com>
2022-10-21 09:36:58 +0100

    leap_year method refactored to one line for all true test expectations

--- lib/leap_year.rb
+++ lib/leap_year.rb
@@ -1,11 +1,9 @@
 def leap_year?(year)
-  if(year) % 100 == (0)
-    false
-  elsif(year) % 4 == (0)
+  if (year.%(4).eql?(0) && year.%(100) != 0)
     true
   else
     false
   end
 end
 
-# Line 2/4 can also be written if year.%.eql?(0) using chaining
+# Line 2/4 can also be written if year.%.eql?(0) using chaining - Refactor!
