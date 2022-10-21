82e0c93ff793c82b4be27052d01968aa1f232b80
David Ewan Campbell <ewan.david@gmail.com>
2022-10-21 09:43:35 +0100

    method added to meet test expectation of leap year divisible by 400 - tests passing

--- lib/leap_year.rb
+++ lib/leap_year.rb
@@ -1,9 +1,7 @@
 def leap_year?(year)
-  if (year.%(4).eql?(0) && year.%(100) != 0)
+  if (year.%(4) == (0) && year.%(100) != 0 || year.%(400) == (0))
     true
   else
     false
   end
 end
-
-# Line 2/4 can also be written if year.%.eql?(0) using chaining - Refactor!
