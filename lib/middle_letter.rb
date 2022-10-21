615b4bf26c6e974f6ba97f1b91c403ba4f9672e3
David Ewan Campbell <ewan.david@gmail.com>
2022-10-21 14:42:39 +0100

    notes added to get_middle.rb file to run in irb but will add to README file when I make push of commits to github

--- lib/middle_letter.rb
+++ lib/middle_letter.rb
@@ -9,3 +9,16 @@ class MiddleLetter
   end
 
 end
+
+=begin
+  To run program in irb:
+  1. Load file with either;
+    load './lib/middle_letter' or require './lib/middle_letter'
+  2. Instantiate an instance of the Middle Letter class with;
+    middleletter = MiddleLetter.new
+  3. Pass an argument to the get_middle method with;
+    middleletter.get_middle('word')
+  4. If word is even in length you will get the 2 middle letters returned,
+    or if word is odd you will get just the middle letter returned
+    i.e. 'grapefruit' will return 'ef', 'cabbage' will return 'b' etc.
+=end
