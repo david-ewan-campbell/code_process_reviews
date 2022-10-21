f354d8b2ae1e379adcf172066f63368b75613098
David Ewan Campbell <ewan.david@gmail.com>
2022-10-21 14:32:17 +0100

    else statement added to method to return middle letter only if word is odd in length - all tests passing

--- lib/middle_letter.rb
+++ lib/middle_letter.rb
@@ -3,6 +3,8 @@ class MiddleLetter
   def get_middle(word)
     if word.length.even?
       return word[word.length/2-1] + word[word.length/2]
+    else
+      return word[word.length/2]
     end
   end
 
