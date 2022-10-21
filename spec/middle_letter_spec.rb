51634270f710a33480e434438cf4d00800523c4f
David Ewan Campbell <ewan.david@gmail.com>
2022-10-21 14:29:41 +0100

    test added to find middle of word if word is odd in length

--- spec/middle_letter_spec.rb
+++ spec/middle_letter_spec.rb
@@ -6,4 +6,8 @@ describe('get_middle') do
   it("returns the middle letters of a word when word is even in length") do
     expect(middleletter.get_middle('elephant')).to(eq("ph"))
   end
+  
+  it("returns the middle letter of a word when word is odd in length") do
+    expect(middleletter.get_middle('giraffe')).to(eq("a"))
+  end
 end
