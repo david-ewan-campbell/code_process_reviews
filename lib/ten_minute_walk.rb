8bda78dddadb98e862426e6479d956189a2cf2a3
David Ewan Campbell <ewan.david@gmail.com>
2022-11-11 12:16:03 +0000

    5th commit - incorrect method to add journey hash, returning expectation of 10 moves rather than if journey does not return to same location, changed method to return true only if both e/w && n/s are equal and therefore return to same location - false test added and working

--- lib/ten_minute_walk.rb
+++ lib/ten_minute_walk.rb
@@ -3,7 +3,7 @@ class TenMinuteWalk
     journey = {w: 0, e: 0, n:0, s:0}
     walk.each {|x| journey[x.to_sym]= journey[x.to_sym]+1}
 
-    if journey[:w] + journey[:e] + journey[:n] + journey[:s] == 10
+    if journey[:w]==journey[:e] && journey[:n]==journey[:s]
       return true
     else
      false
--- spec/ten_minute_walk_spec.rb
+++ spec/ten_minute_walk_spec.rb
@@ -3,9 +3,14 @@ require('ten_minute_walk')
 
 describe TenMinuteWalk do
   describe('#tenminutewalk') do
-    it("returns true when a correct number of ten moves are made") do
+    it("returns true when a correct set of ten directions are made") do
       directions = ['e','e','e','n','w','w','n','w','s','s']
       expect(subject.tenminutewalk(directions)).to eq(true)
+    end
+
+    it("returns false when an incorrect set of ten directions are made") do
+        directions = ['w','s','e','n','n','e','s','w','w','w']
+        expect(subject.tenminutewalk(directions)).to eq(false)
     end
   end
 end
