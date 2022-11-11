cc1d18556c02546477d5401a539e012275ec986f
David Ewan Campbell <ewan.david@gmail.com>
2022-11-11 12:06:49 +0000

    4th commit - if/else statement added to tenminutewalk method to return true if journey length equal to true - passed 9 moves and returns false

--- lib/ten_minute_walk.rb
+++ lib/ten_minute_walk.rb
@@ -2,6 +2,11 @@ class TenMinuteWalk
   def tenminutewalk(walk)
     journey = {w: 0, e: 0, n:0, s:0}
     walk.each {|x| journey[x.to_sym]= journey[x.to_sym]+1}
-    p journey
+
+    if journey[:w] + journey[:e] + journey[:n] + journey[:s] == 10
+      return true
+    else
+     false
+    end
   end
 end
