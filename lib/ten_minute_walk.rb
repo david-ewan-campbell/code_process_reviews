c68dd0c2e86e9560507eef4b395b2141b9d0dda4
David Ewan Campbell <ewan.david@gmail.com>
2022-11-11 11:55:20 +0000

    3rd commit - method now iterating through inputted array and into journey hash that adds up each direction - test failing as not returning true yet but journey iteration method working

--- lib/ten_minute_walk.rb
+++ lib/ten_minute_walk.rb
@@ -1,6 +1,7 @@
 class TenMinuteWalk
-
   def tenminutewalk(walk)
-    true
+    journey = {w: 0, e: 0, n:0, s:0}
+    walk.each {|x| journey[x.to_sym]= journey[x.to_sym]+1}
+    p journey
   end
 end
