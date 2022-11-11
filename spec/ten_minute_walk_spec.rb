ecb1a516a29e724b12f28b5651048d30366a0b24
David Ewan Campbell <ewan.david@gmail.com>
2022-11-11 12:24:59 +0000

    6th commit - added passing test for incorrect journey length - not equalling 10

--- spec/ten_minute_walk_spec.rb
+++ spec/ten_minute_walk_spec.rb
@@ -9,8 +9,13 @@ describe TenMinuteWalk do
     end
 
     it("returns false when an incorrect set of ten directions are made") do
-        directions = ['w','s','e','n','n','e','s','w','w','w']
-        expect(subject.tenminutewalk(directions)).to eq(false)
+      directions = ['w','s','e','n','n','e','s','w','w','w']
+      expect(subject.tenminutewalk(directions)).to eq(false)
+    end
+
+    it("returns false with a journey that is not ten directions in length") do
+      directions = ["s", "w"]
+      expect(subject.tenminutewalk(directions)).to eq(false)
     end
   end
 end
